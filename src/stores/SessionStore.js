f0bfadd7b2dbc13ff06a08b3429b7d9930359e14
Taylor Goolsby <taylorgoolsby@users.noreply.github.com>
2020-05-30 04:26:19 -0700

    chapter navigation

--- src/stores/SessionStore.js
+++ src/stores/SessionStore.js
@@ -4,9 +4,23 @@ import { mobx, kjv } from '../unpkg.js'
 import randomInt from '../functions/randomInt.js'
 
 const { decorate, observable, action, observe, computed } = mobx
-const { verses } = kjv
+
+const { verses, layout } = kjv
+
+const keyToLayout = {}
+for (let i = 0; i < layout.length; i++) {
+  const line = layout[i]
+  if (line[0] === 'VERSE') {
+    keyToLayout[line[1]] = i
+  }
+}
 
 const verseKeys = Object.keys(verses)
+const keyToIndex = {}
+for (let i = 0; i < verseKeys.length; i++) {
+  const key = verseKeys[i]
+  keyToIndex[key] = i
+}
 
 class SessionStore {
   prevVerseIndices /*: Array<number>*/
@@ -14,20 +28,22 @@ class SessionStore {
   userInput /*: string*/
 
   constructor() {
+    // Needed in order to make MobX re-render components:
     observe(this, () => {})
 
     this.prevVerseIndices = []
     this.verseIndex = 0
     this.userInput = ''
 
-    observe(this, 'userInput', ({ newValue }) => {
-      console.log('newValue', newValue)
-    })
     window.addEventListener('keydown', this.handleKeyDown)
   }
 
   handleKeyDown(e /*: KeyboardEvent*/) {
-    if (e.key === 'Tab') {
+    if (/^[a-z0-9 \[\]{};:'",<.>/?|\\!@#$%^&*()_+\-=`~]$/i.test(e.key)) {
+      this.userInput += e.key
+    } else if (e.key === 'Backspace') {
+      this.userInput = this.userInput.slice(0, -1)
+    } else if (e.key === 'Tab') {
       e.preventDefault()
 
       if (!e.shiftKey) {
@@ -35,12 +51,14 @@ class SessionStore {
       } else {
         this.setUndoVerse()
       }
-    } else if (/^[a-z0-9 \[\]{};:'",<.>/?|\\!@#$%^&*()_+\-=`~]$/i.test(e.key)) {
-      console.log(e.key)
     } else if (e.key === 'ArrowRight') {
       this.setNextVerse()
     } else if (e.key === 'ArrowLeft') {
       this.setPrevVerse()
+    } else if (e.key === 'ArrowDown') {
+      this.setNextChapter()
+    } else if (e.key === 'ArrowUp') {
+      this.setPrevChapter()
     }
   }
 
@@ -74,6 +92,55 @@ class SessionStore {
 
   get verse() {
     return verses[verseKeys[this.verseIndex]]
+  }
+
+  setNextChapter() {
+    let layoutIndex = keyToLayout[this.verseKey]
+    while (
+      layoutIndex < layout.length - 1 &&
+      layout[layoutIndex][0] !== 'CHAPTER'
+    ) {
+      layoutIndex++
+    }
+
+    if (layoutIndex === layout.length - 1) {
+      return
+    }
+
+    while (layout[layoutIndex][0] !== 'VERSE') {
+      layoutIndex++
+    }
+    const verseKey = layout[layoutIndex][1]
+    this.prevVerseIndices.push(this.verseIndex)
+    this.verseIndex = keyToIndex[verseKey]
+  }
+
+  setPrevChapter() {
+    let layoutIndex = keyToLayout[this.verseKey]
+    while (layoutIndex > 0 && layout[layoutIndex][0] !== 'CHAPTER') {
+      layoutIndex--
+    }
+
+    if (layoutIndex === 0) {
+      return
+    }
+
+    layoutIndex--
+
+    while (layoutIndex > 0 && layout[layoutIndex][0] !== 'CHAPTER') {
+      layoutIndex--
+    }
+
+    if (layoutIndex === 0) {
+      return
+    }
+
+    while (layout[layoutIndex][0] !== 'VERSE') {
+      layoutIndex++
+    }
+    const verseKey = layout[layoutIndex][1]
+    this.prevVerseIndices.push(this.verseIndex)
+    this.verseIndex = keyToIndex[verseKey]
   }
 }
 
